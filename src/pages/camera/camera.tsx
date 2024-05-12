7af1467399fb6897fbb7da169b179f3aa79cc384
EgorGER <QwertiQQQ@gmail.com>
2024-05-12 06:45:57 +0300

    добовляет блок коментариев

--- src/pages/camera/camera.tsx
+++ src/pages/camera/camera.tsx
@@ -2,27 +2,41 @@ import Header from '../../components/header';
 import Breadcrumbs from '../../components/breadcrumbs';
 import CardRate from '../../components/card-rate';
 import ProductTabs from '../../components/product-tabs';
+import PageContent from '../../components/page-content';
+import Footer from '../../components/footer';
 
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from '../../hooks';
-import { fetchListCameras } from '../../store/api-actions';
+import { useAppSelector} from '../../hooks';
+import { fetchCameraAction } from '../../store/api-actions';
+import { store } from '../../store';
 
 function Camera(): JSX.Element {
+
 
   const params = useParams();
   const cameraId = params.id;
 
-  const dispatch = useAppDispatch();
-  useEffect(()=>{
-    dispatch(fetchListCameras());
-  }, [dispatch]);
+  useEffect(() => {
+    store.dispatch(fetchCameraAction(cameraId));
+  }, [cameraId]);
 
-  const cameras = useAppSelector((state)=>state.cameras);
-  const camera = cameras.filter((item) => item.id === Number(cameraId))[0];
+  const camera = useAppSelector((state)=>state.camera);
+  if(!camera) {
+    return <> Loading</>;
+  }
+
+  console.log(camera);
+
+  // const cameras = useAppSelector((state)=>state.cameras);
+  // const camera = cameras.filter((item) => item.id === Number(cameraId))[0];
+
   const {previewImgWebp, previewImgWebp2x, previewImg2x, rating, name, reviewCount, price} = camera;
 
+
+
   return (
+
     <div className="wrapper">
       <Header />
       <main>
@@ -37,6 +51,8 @@ function Camera(): JSX.Element {
                     <img src="img/content/img1.jpg" srcSet={`${previewImg2x} 2x`} width="560" height="480" alt={name} />
                   </picture>
                 </div>
+
+
                 <div className="product__content">
                   <h1 className="title title--h3">{name}</h1>
                   <CardRate rating={rating} reviewCount={reviewCount} />
@@ -47,16 +63,19 @@ function Camera(): JSX.Element {
                     </svg>Добавить в корзину
                   </button>
                   <ProductTabs camera={camera}/>
-
                 </div>
               </div>
             </section>
-
           </div>
-
-
+          <PageContent />
         </div>
       </main>
+      <a className="up-btn" href="#header">
+        <svg width="12" height="18" aria-hidden="true">
+          <use xlinkHref="#icon-arrow2"></use>
+        </svg>
+      </a>
+      <Footer />
     </div>
   );
 }
