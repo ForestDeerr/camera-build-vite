ff356affde65aed351557e5fc384339f60249ef2
EgorGER <QwertiQQQ@gmail.com>
2024-05-15 20:54:34 +0300

    переночит импорт dayjs в index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app';
 
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+dayjs.locale('ru');
+
 import {Provider} from 'react-redux';
 import { store } from './store';
 
