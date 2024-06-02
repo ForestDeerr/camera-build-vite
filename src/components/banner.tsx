18d54f2257e3667e50dca07e9c4d8b2b5892df9a
EgorGER <QwertiQQQ@gmail.com>
2024-06-02 22:38:32 +0300

    устанавливает Swiper и подключает его на страницу

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,15 +1,48 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination} from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 
 function Banner(): JSX.Element {
 
   return (
-    <div className="banner">
-      <picture>
-        <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x" />
-        <img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер" />
-      </picture>
-      <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
-    </div>
+    <Swiper
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Autoplay, Pagination]}
+      className="mySwiper"
+    >
+
+      <SwiperSlide>
+        <div className="banner">
+          <picture>
+            <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x" />
+            <img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер" />
+          </picture>
+          <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="banner">
+          <picture>
+            <source type="image/webp" srcSet="img/content/van-shot.webp, img/content/promo@2x.webp 2x" />
+            <img src="img/content/promo.jpg" srcSet="img/content/promo@2x.jpg 2x" width="1280" height="280" alt="баннер" />
+          </picture>
+          <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+
+
   );
 }
-
 export default Banner;
